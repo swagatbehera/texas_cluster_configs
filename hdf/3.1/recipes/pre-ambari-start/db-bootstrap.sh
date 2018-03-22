d4a1fe82e777cc2c4b2288b6c8bda2566131ddd2
Gergely Novak <gnovak@hortonworks.com>
2018-03-22 11:32:07 +0100

    SUST-490. Add Openstack support to Texas Cluster Creator
    Change-Id: Id2a20e24729918827687bb63ec79e908c05ca63e

--- hdf/3.1/recipes/pre-ambari-start/db-bootstrap.sh
+++ hdf/3.1/recipes/pre-ambari-start/db-bootstrap.sh
@@ -1,100 +1,33 @@
 #!/bin/bash
 
 
-# function to create mysql-community repo file
-create_mysql_community_repo()
-{
-
-	cat > /etc/yum.repos.d/maria.repo << MYSQLEOF
+create_mysql_community_repo() {
+	cat > /etc/yum.repos.d/mysql-community.repo << MYSQLEOF
 [mysql-connectors-community]
 name=MySQL Connectors Community
-baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/$basearch/
+baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/\$basearch/
 enabled=1
 gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql
 
 [mysql-tools-community]
 name=MySQL Tools Community
-baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/$basearch/
+baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/\$basearch/
 enabled=1
 gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql
 
-# Enable to use MySQL 5.5
-[mysql55-community]
-name=MySQL 5.5 Community Server
-baseurl=http://repo.mysql.com/yum/mysql-5.5-community/el/7/$basearch/
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-
-# Enable to use MySQL 5.6
 [mysql56-community]
 name=MySQL 5.6 Community Server
-baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/
+baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/\$basearch/
 enabled=1
 gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql
 
-# Note: MySQL 5.7 is currently in development. For use at your own risk.
-# Please read with sub pages: https://dev.mysql.com/doc/relnotes/mysql/5.7/en/
-[mysql57-community-dmr]
-name=MySQL 5.7 Community Server Development Milestone Release
-baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
 MYSQLEOF
-
 }
 
-# function to create mysql-community-source repo file
-create_mysql_community_source_repo()
-{
-
-	cat > /etc/yum.repos.d/maria.repo << MYSQLSEOF
-[mysql-connectors-community-source]
-name=MySQL Connectors Community - Source
-baseurl=http://repo.mysql.com/yum/mysql-connectors-community/el/7/SRPMS
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-
-[mysql-tools-community-source]
-name=MySQL Tools Community - Source
-baseurl=http://repo.mysql.com/yum/mysql-tools-community/el/7/SRPMS
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-
-[mysql55-community-source]
-name=MySQL 5.5 Community Server - Source
-baseurl=http://repo.mysql.com/yum/mysql-5.5-community/el/7/SRPMS
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-
-[mysql56-community-source]
-name=MySQL 5.6 Community Server - Source
-baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/SRPMS
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-
-[mysql57-community-dmr-source]
-name=MySQL 5.7 Community Server Development Milestone Release - Source
-baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/SRPMS
-enabled=0
-gpgcheck=1
-gpgkey=file:/etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-MYSQLSEOF
-}
-
-
-# create db init script
-create_db_init_sql_file()	
-{
-
+create_db_init_sql_file() {
 	cat > /tmp/db-script.sql <<ENDDBSQLOF
 # TODO fix this to only grant for specific db and also does not work if Ranger is on the same host as the DB
 # ranger db setup
@@ -136,7 +69,7 @@ CREATE USER 'streamline'@'localhost' IDENTIFIED BY 'streamline';
 GRANT ALL PRIVILEGES ON streamline.* TO 'streamline'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON streamline.* TO 'streamline'@'localhost' IDENTIFIED BY 'streamline' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
- 
+
 #create database for registry
 CREATE DATABASE registry;
 CREATE USER 'registry'@'%' IDENTIFIED BY 'registry';
@@ -144,50 +77,29 @@ CREATE USER 'registry'@'localhost' IDENTIFIED BY 'registry';
 GRANT ALL PRIVILEGES ON registry.* TO 'registry'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON registry.* TO 'registry'@'localhost' IDENTIFIED BY 'registry' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
 ENDDBSQLOF
-
 }
 
-# install mysql-server
-install_mysql_server()
-{
-
-	yum -y install mysql-community-release-el7-5.noarch
-	yum -y install mysql-community-common-5.6.38-2.el7.x86_64
-	yum -y install mysql-community-libs-5.6.38-2.el7.x86_64
-	yum -y install mysql-community-client-5.6.38-2.el7.x86_64
-	yum -y install mysql-community-server-5.6.38-2.el7.x86_64
-
+install_mysql_server() {
+	yum -y install mysql-community-release
+	yum -y install mysql-community-server
 }
 
-
-# start mysql-server
-start_mysql_server()
-{
-
+start_mysql_server() {
 	service mysqld start
 }
 
-# run db bootstrap script
-init_db_bootstrap_script()
-{
-
+init_db_bootstrap_script() {
 	mysql -u root < /tmp/db-script.sql
-
 }
 
-# main function
-main()
-{
-
+main() {
 	create_mysql_community_repo
-	create_mysql_community_source_repo
 	create_db_init_sql_file
 	install_mysql_server
 	start_mysql_server
 	init_db_bootstrap_script
 }
 
-
-# start main
 main
